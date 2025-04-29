5525f1d1a1c61a72e89c8a9b8723b1268ee44777
zifo-10 <ahmedmohammed.sh10@gmail.com>
2025-04-29 03:09:21 +0300

    feat: login and job assessment

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,7 @@
 import os
 from typing import List
 from fastapi.responses import JSONResponse
-
+from bson import ObjectId
 from pyobjectID import PyObjectId
 from dotenv import load_dotenv
 from fastapi import FastAPI, HTTPException, Query
@@ -9,7 +9,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from app.client.mongo_client import MongoDBClient
 from app.model.assessment_dto import GetAssessmentDTO
-from app.model.job_dto import GetJobDTO
+from app.model.job_dto import GetJobDTO, TrainingDTO
 
 app = FastAPI()
 
@@ -26,9 +26,7 @@ app.add_middleware(
 
 mongo_uri = os.getenv('MONGO_URI')
 db_name = os.getenv('DB_NAME', 'assessment')
-job_collection = os.getenv('COLLECTION_NAME', 'new_job')
-assessment_collection = 'assessment'
-scenario_collection = 'scenario'
+job_collection = os.getenv('COLLECTION_NAME', 'new_job_with_questions')
 
 mongo_client = MongoDBClient(mongo_uri, db_name)
 
@@ -36,111 +34,53 @@ mongo_client = MongoDBClient(mongo_uri, db_name)
 def healthcheck():
     return JSONResponse(status_code=200, content={"message": "healthy"})
 
-@app.get("/job", response_model=List[GetJobDTO])
-async def list_jobs(limit: int = Query(10, ge=1), skip: int = Query(0, ge=0)):
+
+@app.post("/register")
+async def register_user(email: str, password: str):
     try:
-        job_list = []
-        # Pass the limit and skip to the find method
-        jobs = mongo_client.find(
+        insert_user = mongo_client.insert_one(
+            collection_name='users',
+            document={
+                "email": email,
+                "password": password
+            }
+        )
+        return {'user_id': str(insert_user)}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+@app.post("/login")
+async def login_user(email: str, password: str):
+    try:
+        user = mongo_client.find_one(
+            collection_name='users',
+            query={
+                "email": email,
+                "password": password
+            }
+        )
+        if not user:
+            raise HTTPException(status_code=401, detail="Invalid credentials")
+        return {'user_id': str(user['_id'])}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+@app.get("/job/{job_code}")
+async def get_job(job_code: int):
+    try:
+        job = mongo_client.find_one(
             job_collection,
-            {},
-            limit=limit,
-            skip=skip
+            query={"job_code": job_code}
         )
-
-        for job in jobs:
-            job_list.append(GetJobDTO(**job))
-
-        return job_list
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
-
-
-@app.get("/job_id/{job_id}/{level}")
-async def get_assessment(job_id: str,
-                        level: int):
-    try:
-        quiz = mongo_client.find_one(
-            assessment_collection,
-            {"job_id": job_id,
-                "level": level}
+        print(job)
+        get_train = mongo_client.find(
+            'train',
+            query={"job_id": ObjectId(job['_id'])}
         )
-        if not quiz:
-            raise HTTPException(status_code=404, detail="Quiz not found")
-        # Convert ObjectId to string for JSON serialization
-        quiz = GetAssessmentDTO(**quiz)
-        return quiz
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
-
-
-@app.get("/assessment/{item_id}")
-async def get_assessment_with_scenario(item_id: PyObjectId):
-    # Step 1: Convert the item_id to ObjectId
-    # Step 2: Get the assessment document
-    assessment_pipeline = [{"$match": {"_id": item_id}}]
-    assessment_docs = mongo_client.aggregate(assessment_collection, assessment_pipeline)
-
-    if not assessment_docs:
-        raise HTTPException(status_code=404, detail="Assessment not found")
-
-    assessment_doc = assessment_docs[0]
-
-    # Step 3: Extract first valid option_id from any question
-    try:
-        first_option_id = None
-        questions = assessment_doc.get("assessment", {}).get("questions", [])
-        scenario_q = questions[-1]
-        options = scenario_q["options"]
-        options_id_list = []
-        for i in options:
-            i["option_id"] = str(i["option_id"])
-            options_id_list.append(i)
-        scenario_q["options"] = options_id_list
-
-        return scenario_q
-    except Exception as e:
-        raise e
-
-
-@app.get("/scenario-base-q/{item_id}")
-async def get_scenario_base_question(item_id: PyObjectId):
-    try:
-        scenario_base = mongo_client.find_one(
-            scenario_collection,
-            {"parent_option_id": item_id}
-        )
-
-        if not scenario_base:
-            raise HTTPException(status_code=404, detail="Scenario not found")
-
-        for option in scenario_base.get("options", []):
-            option["option_id"] = str(option["option_id"])
-
-        scenario_base["parent_option_id"] = str(scenario_base["parent_option_id"])
-        scenario_base["_id"] = str(scenario_base["_id"])
-
-        return scenario_base
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
-
-
-@app.get("/prompt")
-async def list_prompt(limit: int = Query(10, ge=1), skip: int = Query(0, ge=0)):
-    try:
-        prompt_list = []
-        # Pass the limit and skip to the find method
-        prompts = mongo_client.find(
-            "prompt",
-            {},
-            limit=limit,
-            skip=skip
-        )
-
-        for prompt in prompts:
-            prompt['_id'] = str(prompt['_id'])
-            prompt_list.append(prompt)
-
-        return prompt_list
+        training_list = []
+        for train in get_train:
+            training_list.append(TrainingDTO(**train))
+        return training_list
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
